5c4a2c67c9b92b036bd60b095d063a14f11cc896
Dale Belcham <dandelionwishes@protonmail.ch>
2023-08-16 18:48:32 +0200

    Add code comparing methods

--- master_mind.rb
+++ master_mind.rb
@@ -1,43 +1,57 @@
 class Computer
   attr_reader :computers_code
 
-  def initialize(computers_code)
-    @computers_code = computers_code
+  def initialize
+    @computers_code = generate_color_sequence
+  end
+
+  def generate_color_sequence
+    colors = %w[red green blue orange purple yellow]
+    new_color_hash = { first: '', second: '', third: '', fourth: '' }
+    new_color_hash.transform_values! { colors[rand(0..5)] }
   end
 end
 
 class Player
   attr_accessor :players_code
 
-  def initialize(players_code)
-    @players_code = players_code
+  def initialize
+    @players_code = { first: '', second: '', third: '', fourth: '' }
+  end
+
+  def choose_sequence
+    @players_code.transform_values! do
+      puts 'Player, please choose a color: ( red, yellow, green, blue, orange, or purple )'
+      choice = gets.chomp!
+      choice
+    end
   end
 end
 
-def generate_color_sequence
-  colors = %w[red green blue orange purple yellow]
-  new_color_array = []
-
-  4.times do
-    index = rand(0..5)
-    new_color_array.unshift(colors[index])
-  end
-  new_color_array
+def check_right_place(hash1, hash2)
+  right_place = hash1.map { |pos, value| value == hash2[pos] }
+  right_place.tally
 end
 
-def choose_sequence
-  players_code = []
-
-  4.times do
-    puts 'Player, please choose a color: ( red, yellow, green, blue, orange, or purple )'
-    choice = gets.chomp!
-    players_code.unshift(choice)
-  end
-  players_code
+def check_right_color(hash1, hash2)
+  new_arr = hash1.values.intersection(hash2.values)
+  new_arr.length
 end
 
-computer = Computer.new(generate_color_sequence)
+def display_results(result1, result2)
+  if result1[true] == 4
+    return puts 'Congrats! You guessed the secret code.'
+  elsif result1.key?(true)
+    puts "Correct colors in correct places: #{result1[true]}"
+  end
+
+  puts result2.positive? ? "Correct colors: #{result2}" : 'No correct colors'
+end
+
+
+computer = Computer.new
 p computer.computers_code
 
-player = Player.new(choose_sequence)
+player = Player.new
+player.choose_sequence
 p player.players_code
